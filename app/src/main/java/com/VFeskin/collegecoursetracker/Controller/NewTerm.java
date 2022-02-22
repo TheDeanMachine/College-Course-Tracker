162e70e6698dfbf2e387b85c5d0604ead8088674
Vadym Feskin <Vadym.feskin@gmail.com>
2022-02-21 16:22:27 -0800

    Fixed R file issue

---
+++ app/src/main/java/com/VFeskin/collegecoursetracker/Controller/NewTerm.java
@@ -0,0 +1,16 @@
+package com.VFeskin.collegecoursetracker.Controller;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.VFeskin.collegecoursetracker.R;
+
+public class NewTerm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_term);
+
+    }
+
+}
