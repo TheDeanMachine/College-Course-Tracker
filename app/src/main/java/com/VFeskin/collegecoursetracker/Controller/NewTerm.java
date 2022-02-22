1843e1f53716a7896c6927ecb7127f09411eaa5e
Vadym Feskin <Vadym.feskin@gmail.com>
2022-02-22 10:11:59 -0800

    basic outline for New Term

--- app/src/main/java/com/VFeskin/collegecoursetracker/Controller/NewTerm.java
+++ app/src/main/java/com/VFeskin/collegecoursetracker/Controller/NewTerm.java
@@ -1,16 +1,64 @@
 package com.VFeskin.collegecoursetracker.Controller;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.VFeskin.collegecoursetracker.Model.Term;
+import com.VFeskin.collegecoursetracker.Model.TermViewModel;
 import com.VFeskin.collegecoursetracker.R;
 
 public class NewTerm extends AppCompatActivity {
+
+    private EditText termTitle;
+    private EditText startDate;
+    private EditText endDate;
+    private Button createTerm;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_term);
+        // set the field values to the xml id
+        termTitle = findViewById(R.id.editTextTermTitle);
+        startDate = findViewById(R.id.editTextStartDate);
+        endDate = findViewById(R.id.editTextEndDate);
+        createTerm = findViewById(R.id.createTermButton);
+
+//        createTerm.setOnClickListener(view -> {
+//
+//            String title = termTitle.getText().toString();
+//            String start = startDate.getText().toString();
+//            String end = endDate.getText().toString();
+//            Term term = new Term(title, start, end);
+//
+//            TermViewModel.insert(term);
+//
+//
+//        });
 
     }
+
+
+    public void onClickCreateTerm(View view) {
+        if(!TextUtils.isEmpty(termTitle.getText().toString())) {
+            String title = termTitle.getText().toString();
+            String start = startDate.getText().toString();
+            String end = endDate.getText().toString();
+
+            Term term = new Term(title, start, end);
+
+            TermViewModel.insert(term);
+
+        } else {
+            Toast.makeText(this, R.string.emptyInput, Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
 
 }
