c0f2b487ac3394180a1e358c685ade6b950be2b1
Vadym Feskin <Vadym.feskin@gmail.com>
2022-02-25 11:14:33 -0800

    working new course activity

---
+++ app/src/main/java/com/VFeskin/collegecoursetracker/Controller/NewCourse.java
@@ -0,0 +1,20 @@
+package com.VFeskin.collegecoursetracker.Controller;
+
+import com.VFeskin.collegecoursetracker.R;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewCourse extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_course);
+
+
+
+
+    }
+}
