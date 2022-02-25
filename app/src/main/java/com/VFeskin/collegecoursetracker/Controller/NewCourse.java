d82434ce17659b316d77d342603f0fad1fcf3001
Vadym Feskin <Vadym.feskin@gmail.com>
2022-02-25 14:37:07 -0800

    working new course class

--- app/src/main/java/com/VFeskin/collegecoursetracker/Controller/NewCourse.java
+++ app/src/main/java/com/VFeskin/collegecoursetracker/Controller/NewCourse.java
@@ -2,10 +2,39 @@ package com.VFeskin.collegecoursetracker.Controller;
 
 import com.VFeskin.collegecoursetracker.R;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * This class is used for adding a new term.
+ * Displays a form that the user can fill out, uses the information entered
+ * to create a new term object/entity and add it into the database.
+ */
 public class NewCourse extends AppCompatActivity {
 
+    // XML attributes
+    private EditText courseTitleTxt;
+    private EditText startDateTxt;
+    private EditText endDateTxt;
+    private Spinner courseStatusSpinner;
+    private EditText instructorNameTxt;
+    private EditText instructorPhoneTxt;
+    private EditText instructorEmailTxt;
+    private Button createCourseButton;
+
+    // date related fields
+    private final Calendar calendar = Calendar.getInstance();
+    DatePickerDialog.OnDateSetListener dateDialog;
+    DatePickerDialog.OnDateSetListener dateDialog2;
+    private Date startDate;
+    private Date endDate;
 
 
     @Override
@@ -13,7 +42,9 @@ public class NewCourse extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_course);
 
-
+        // set the field values to the xml ids
+        courseTitleTxt = findViewById(R.id.editTextCourseTitle);
+        startDateTxt = findViewById(R.id.editTextCourseStartDate);
 
 
     }
