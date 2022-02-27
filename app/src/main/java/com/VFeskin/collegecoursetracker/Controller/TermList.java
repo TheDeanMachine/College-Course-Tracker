66509bd7e4b04aa0f6b3a91cc59329fadc06f15a
Vadym Feskin <Vadym.feskin@gmail.com>
2022-02-27 14:18:48 -0800

    refactored hierarchy

---
+++ app/src/main/java/com/VFeskin/collegecoursetracker/Controller/TermList.java
@@ -0,0 +1,14 @@
+package com.VFeskin.collegecoursetracker.Controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.VFeskin.collegecoursetracker.R;
+import android.os.Bundle;
+
+public class TermList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_term_list);
+    }
+}
---
+++ app/src/main/java/com/VFeskin/collegecoursetracker/HomeScreen.java
@@ -0,0 +1,21 @@
+package com.VFeskin.collegecoursetracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import android.os.Bundle;
+import android.util.Log;
+import com.VFeskin.collegecoursetracker.Model.TermViewModel;
+
+
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_screen);
+
+
+
+    }
+}
