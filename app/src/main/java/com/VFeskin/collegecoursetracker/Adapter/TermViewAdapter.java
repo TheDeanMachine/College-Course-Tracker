41839ba8e75e8b0f55bfd1b6c6a09ec3185354ef
Vadym Feskin <Vadym.feskin@gmail.com>
2022-02-28 10:28:29 -0800

    successful test of RecycleView for Term

--- app/src/main/java/com/VFeskin/collegecoursetracker/Adapter/TermViewAdapter.java
+++ app/src/main/java/com/VFeskin/collegecoursetracker/Adapter/TermViewAdapter.java
@@ -1,34 +1,69 @@
 package com.VFeskin.collegecoursetracker.Adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.VFeskin.collegecoursetracker.Model.Term;
+import com.VFeskin.collegecoursetracker.R;
+
+import java.util.List;
 
 
 public class TermViewAdapter extends RecyclerView.Adapter<TermViewAdapter.ViewHolder> {
+
+    private LiveData<List<Term>> termList;
+
+
+
+    public TermViewAdapter(LiveData<List<Term>> termList) {
+        this.termList = termList;
+    }
+
+
     @NonNull
     @Override
     public TermViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        // term card view
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.term_card, parent, false);
+
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull TermViewAdapter.ViewHolder holder, int position) {
+        Term term = termList.getValue().get(position);
+        holder.title.setText(term.getTitle());
+        holder.start.setText(term.getStartDate().toString());
+        holder.end.setText(term.getEndDate().toString());
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return termList.getValue().size(); //Attempt to invoke interface method 'int java.util.List.size()' on a null object reference
     }
 
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
+        public TextView title;
+        public TextView start;
+        public TextView end;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            title = itemView.findViewById(R.id.textViewTermTitle);
+            start = itemView.findViewById(R.id.textViewTermStartDate);
+            end = itemView.findViewById(R.id.textViewTermEndDate);
+
+
         }
     }
 }
--- app/src/main/java/com/VFeskin/collegecoursetracker/Controller/TermList.java
+++ app/src/main/java/com/VFeskin/collegecoursetracker/Controller/TermList.java
@@ -1,14 +1,56 @@
 package com.VFeskin.collegecoursetracker.Controller;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.VFeskin.collegecoursetracker.Adapter.TermViewAdapter;
+import com.VFeskin.collegecoursetracker.Model.Term;
+import com.VFeskin.collegecoursetracker.Model.TermViewModel;
 import com.VFeskin.collegecoursetracker.R;
 import android.os.Bundle;
+import java.util.List;
 
 public class TermList extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private TermViewAdapter termViewAdapter;
+    private LiveData<List<Term>> termList;
+
+    TermViewModel termViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_term_list);
+
+        recyclerView = findViewById(R.id.recycler_view_term);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+
+        // creates an instance of view model to use
+        termViewModel = new ViewModelProvider.AndroidViewModelFactory(TermList.this
+                .getApplication())
+                .create(TermViewModel.class);
+
+        termList = termViewModel.getAllTerms();
+
+        // observer
+        termViewModel.getAllTerms().observe(this, terms -> {
+
+            termViewAdapter = new TermViewAdapter(termList);
+
+            recyclerView.setAdapter(termViewAdapter);
+        });
+
+
+
+
+
+
+
     }
 }
