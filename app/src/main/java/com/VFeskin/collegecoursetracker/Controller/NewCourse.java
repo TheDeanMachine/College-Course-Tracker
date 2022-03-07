be4d3f365408915cf9ae0edcc3d82e5a45b31411
Vadym Feskin <Vadym.feskin@gmail.com>
2022-03-07 15:52:37 -0800

    added input validation

--- app/src/main/java/com/VFeskin/collegecoursetracker/Controller/NewCourse.java
+++ app/src/main/java/com/VFeskin/collegecoursetracker/Controller/NewCourse.java
@@ -4,6 +4,8 @@ import com.VFeskin.collegecoursetracker.Model.Course;
 import com.VFeskin.collegecoursetracker.Model.CourseViewModel;
 import com.VFeskin.collegecoursetracker.R;
 import com.VFeskin.collegecoursetracker.Utility.Status;
+import com.google.android.material.snackbar.Snackbar;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 import android.app.DatePickerDialog;
@@ -58,6 +60,7 @@ public class NewCourse extends AppCompatActivity {
         startDateTxt = findViewById(R.id.editTextCourseStartDate);
         endDateTxt = findViewById(R.id.editTextCourseEndDate);
         courseStatusSpinner = findViewById(R.id.spinner);
+        courseStatusSpinner.setPrompt("Please Select status");
 
 //        // create an array with Enum values for the spinner to hold
 //        courseStatusSpinner.setAdapter(new ArrayAdapter<>(this,
@@ -88,36 +91,99 @@ public class NewCourse extends AppCompatActivity {
                 .show());
 
         // gets the values from date picker, onDataSet
-        dateDialog = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int day) {
-                calendar.set(year, month, day);
-                startDate = calendar.getTime();
-                // format the output the screen
-                startDateTxt.setText(DateFormat.getDateInstance(DateFormat.MEDIUM).format(startDate));
-            }
+        dateDialog = (view, year, month, day) -> {
+            calendar.set(year, month, day);
+            startDate = calendar.getTime();
+            // format the output the screen
+            startDateTxt.setError(null); // clears set error
+            startDateTxt.setText(DateFormat.getDateInstance(DateFormat.MEDIUM).format(startDate));
         };
 
         // gets the values from date picker, onDataSet
-        dateDialog2 = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int day) {
-                calendar.set(year, month, day);
-                endDate = calendar.getTime();
-                // format the output the screen
-                endDateTxt.setText(DateFormat.getDateInstance(DateFormat.MEDIUM).format(endDate));
-            }
+        dateDialog2 = (view, year, month, day) -> {
+            calendar.set(year, month, day);
+            endDate = calendar.getTime();
+            // format the output the screen
+            endDateTxt.setError(null); // clears set error
+            endDateTxt.setText(DateFormat.getDateInstance(DateFormat.MEDIUM).format(endDate));
         };
 
         // collect input and create new course
         createCourseButton.setOnClickListener(view -> {
 
-            //TODO : input validation
-            String title = courseTitleTxt.getText().toString();
-            String status = courseStatusSpinner.getSelectedItem().toString();
-            String name = instructorNameTxt.getText().toString();
-            String phone = instructorPhoneTxt.getText().toString();
-            String email = instructorEmailTxt.getText().toString();
+            // Input validation
+            String title = null;
+            try {
+                title = courseTitleTxt.getText().toString();
+                if(title == null || title.isEmpty()) {
+                    throw new Exception();
+                }
+            } catch (Exception e) {
+                courseTitleTxt.setError("Title is required!");
+                Snackbar.make(view, "Please enter a title", Snackbar.LENGTH_SHORT).show();
+                return;
+            }
+
+            if (startDate == null || startDateTxt.getText().toString().isEmpty()) {
+                startDateTxt.setError("Start date is required!");
+                Snackbar.make(view, "Please enter a date", Snackbar.LENGTH_SHORT).show();
+                return;
+            }
+
+            if (endDate == null || endDateTxt.getText().toString().isEmpty()) {
+                endDateTxt.setError("End date is required!");
+                Snackbar.make(view, "Please enter a date", Snackbar.LENGTH_SHORT).show();
+                return;
+            }
+
+            String name = null;
+            try {
+                name = instructorNameTxt.getText().toString();
+                if(name == null || name.isEmpty()) {
+                    throw new Exception();
+                }
+            } catch (Exception e) {
+                instructorNameTxt.setError("Name is required!");
+                Snackbar.make(view, "Please enter the instructor name", Snackbar.LENGTH_SHORT).show();
+                return;
+            }
+
+            String phone = null;
+            try {
+               phone = instructorPhoneTxt.getText().toString();
+                if(phone == null || phone.isEmpty()) {
+                    throw new Exception();
+                }
+            } catch (Exception e) {
+                instructorPhoneTxt.setError("Phone is required!");
+                Snackbar.make(view, "Please enter the instructor phone", Snackbar.LENGTH_SHORT).show();
+                return;
+            }
+
+            String email = null;
+            try {
+                email = instructorEmailTxt.getText().toString();
+                if(email == null || email.isEmpty()) {
+                    throw new Exception();
+                }
+            } catch (Exception e) {
+                instructorEmailTxt.setError("Email is required!");
+                Snackbar.make(view, "Please enter the instructor email", Snackbar.LENGTH_SHORT).show();
+                return;
+            }
+
+            String status = null;
+
+            try {
+                status = courseStatusSpinner.getSelectedItem().toString();
+                if (status == null || courseStatusSpinner.getSelectedItem().equals("Select status")) {
+                    throw new Exception();
+                }
+            } catch (Exception e) {
+                Snackbar.make(view, "Please course status", Snackbar.LENGTH_SHORT).show();
+                return;
+            }
+
             id = getIntent().getIntExtra("ID", 0);
 
             CourseViewModel.insert(new Course(title, startDate, endDate, name, phone, email, status, id));
